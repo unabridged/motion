a8cc2ac8c638d8dcdd5816cf503808c5662fe013
Alec Larsen <alec@unabridgedsoftware.com>
2020-06-24 18:03:56 -0700

    Add coverage guard

--- spec/support/coverage_report.rb
+++ spec/support/coverage_report.rb
@@ -6,3 +6,7 @@ SimpleCov.start do
   add_filter "/bin/"
   add_filter "/spec/"
 end
+
+# TODO: Update this to branch coverge when we upgrade to v0.18
+# https://github.com/colszowka/simplecov#minimum-coverage
+SimpleCov.minimum_coverage(100)
