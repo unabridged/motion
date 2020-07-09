46f1686fa2487a89803aaf9f177ef9d4dffec933
Alec Larsen <alec@unabridgedsoftware.com>
2020-07-09 08:53:19 -0700

    Reformat and cleanup error messages (#28)

--- lib/motion/component/lifecycle.rb
+++ lib/motion/component/lifecycle.rb
@@ -10,13 +10,11 @@ module Motion
       extend ActiveSupport::Concern
 
       class_methods do
-        # TODO: "IncorrectRevisionError" doesn't make sense for this anymore.
-        # It should probably be something like "CannotUpgrade" and the error
-        # message should focus on how to handle deployments gracefully.
-        def upgrade_from(previous_revision, _instance)
-          raise IncorrectRevisionError.new(
-            Motion.config.revision,
-            previous_revision
+        def upgrade_from(previous_revision, instance)
+          raise UpgradeNotImplementedError.new(
+            instance,
+            previous_revision,
+            Motion.config.revision
           )
         end
       end
