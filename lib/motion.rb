62e392ef8724a78a6f9d8f624bffd9ae11411a56
Alec Larsen <alec@unabridgedsoftware.com>
2020-09-08 21:15:28 -0700

    Officially support Ruby 2.4

--- lib/motion.rb
+++ lib/motion.rb
@@ -19,41 +19,43 @@ module Motion
   autoload :Serializer, "motion/serializer"
   autoload :TestHelpers, "motion/test_helpers"
 
-  def self.configure(&block)
-    raise AlreadyConfiguredError if @config
+  class << self
+    def configure(&block)
+      raise AlreadyConfiguredError if @config
 
-    @config = Configuration.new(&block)
-  end
+      @config = Configuration.new(&block)
+    end
 
-  def self.config
-    @config ||= Configuration.default
-  end
+    def config
+      @config ||= Configuration.default
+    end
 
-  singleton_class.alias_method :configuration, :config
+    alias configuration config
 
-  def self.serializer
-    @serializer ||= Serializer.new
-  end
+    def serializer
+      @serializer ||= Serializer.new
+    end
 
-  def self.markup_transformer
-    @markup_transformer ||= MarkupTransformer.new
-  end
+    def markup_transformer
+      @markup_transformer ||= MarkupTransformer.new
+    end
 
-  def self.build_renderer_for(websocket_connection)
-    config.renderer_for_connection_proc.call(websocket_connection)
-  end
+    def build_renderer_for(websocket_connection)
+      config.renderer_for_connection_proc.call(websocket_connection)
+    end
 
-  def self.notify_error(error, message)
-    config.error_notification_proc&.call(error, message)
-  end
+    def notify_error(error, message)
+      config.error_notification_proc&.call(error, message)
+    end
 
-  # This method only exists for testing. Changing configuration while Motion is
-  # in use is not supported. It is only safe to call this method when no
-  # components are currently mounted.
-  def self.reset_internal_state_for_testing!(new_configuration = nil)
-    @config = new_configuration
-    @serializer = nil
-    @markup_transformer = nil
+    # This method only exists for testing. Changing configuration while Motion
+    # is in use is not supported. It is only safe to call this method when no
+    # components are currently mounted.
+    def reset_internal_state_for_testing!(new_configuration = nil)
+      @config = new_configuration
+      @serializer = nil
+      @markup_transformer = nil
+    end
   end
 end
 
